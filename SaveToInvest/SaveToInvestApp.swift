919ca299c7a094daf291bdd70ae3821b4b990aa2
Kesong Lin <kvl5742@psu.edu>
2025-03-14 12:18:08 -0400

    Initial Commit

---
+++ SaveToInvest/SaveToInvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveToInvestApp.swift
+//  SaveToInvest
+//
+//  Created by Kesong Lin on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SaveToInvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
